579a4e70ed2ba5c81ed491b9d6958b29e39d9548
Elias Hernandis <eliashernandis@gmail.com>
2020-02-26 13:46:08 +0100

    Update Core23 to match standard marker names

--- R/kits.R
+++ R/kits.R
@@ -6,7 +6,7 @@ analysis_kits <-
       markers = c("CSF1PO", "D1S1656", "D2S441", "D2S1338", "D3S1358", "D5S818",
                   "D7S820", "D8S1179", "D10S1248", "D12S391", "D13S317", "D16S539",
                   "D18S51", "D19S433", "D21S11", "D22S1045", "FGA", "TH01", "TPOX",
-                  "vWA", "SE33", "Penta_D", "Penta_E")
+                  "VWA", "SE33", "Penta_D", "Penta_E")
     ),
 
     list(
@@ -15,7 +15,7 @@ analysis_kits <-
       markers = c("CSF1PO", "D1S1656", "D2S441", "D2S1338", "D3S1358", "D5S818",
                   "D7S820", "D8S1179", "D10S1248", "D12S391", "D13S317", "D16S539",
                   "D18S51", "D19S433", "D21S11", "D22S1045", "FGA", "TH01", "TPOX",
-                  "vWA", "SE33", "Penta_D", "Penta_E", "D6S1043")
+                  "VWA", "SE33", "Penta_D", "Penta_E", "D6S1043")
     ),
 
     list(
